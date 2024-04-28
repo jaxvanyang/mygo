aa6d2aa80351d60d0ce3f167e73e02510a2b9af3
Jax Young <jaxvanyang@gmail.com>
2024-04-28 15:10:29 +0800

    feat: add Monte Carlo tree search bot

--- src/agent/naive.py
+++ src/agent/naive.py
@@ -2,10 +2,11 @@
 
 import math
 import random
+from copy import deepcopy
 
 from agent.base import Agent
 from go.game import Game
-from go.types import Move
+from go.types import Color, Move
 
 
 class RandomBot(Agent):
@@ -73,9 +74,121 @@ class TreeSearchBot(Agent):
 
         for move in game.good_moves:
             move_score = self.calc_move_score(game, move, self.depth)
-
             if move_score > best_move_score:
                 best_move_score = move_score
                 best_move = move
 
         return best_move
+
+
+class MCTSNode:
+    """Monte Carlo tree node."""
+
+    def __init__(self, game: Game, parent=None) -> None:
+        self.game = game
+        self.parent = parent
+        self.children = []
+        self.black_wins = 0
+        self.white_wins = 0
+        good_moves = game.good_moves
+        self.unvisited_moves = good_moves if good_moves else [Move.pass_()]
+
+    def __repr__(self) -> str:
+        return f"MCTSNode({self.game!r}, {self.parent!r})"
+
+    @property
+    def count(self) -> int:
+        return self.black_wins + self.white_wins
+
+    @property
+    def win_rate(self) -> float:
+        assert self.count >= 0.0
+        if self.game.next_color == Color.black:
+            return self.white_wins / self.count
+        else:
+            return self.black_wins / self.count
+
+    def uct_score(self, child, total_count: int, temp: float) -> float:
+        """Upper Confidence bound for Trees formula."""
+        w = child.win_rate
+        n = child.count
+
+        return w + temp * math.sqrt(math.log2(total_count) / n)
+
+    def select_child(self, temp: float):
+        if self.unvisited_moves:
+            game = deepcopy(self.game)
+            game.apply_move(self.unvisited_moves.pop())
+            child = MCTSNode(game, self)
+            self.children.append(child)
+            return child
+
+        total_count = sum(child.count for child in self.children)
+        best_score = -1.0
+        best_child = None
+
+        for child in self.children:
+            score = self.uct_score(child, total_count, temp)
+            if score > best_score:
+                best_score = score
+                best_child = child
+
+        assert isinstance(best_child, MCTSNode)
+        return best_child
+
+    def update(self, winner: Color) -> None:
+        """Update winner counts."""
+        if winner == Color.black:
+            self.black_wins += 1
+        elif winner == Color.white:
+            self.white_wins += 1
+
+    def simulate(self, temp: float) -> None:
+        """Simulate a random game."""
+
+        if self.game.is_over:
+            winner = self.game.winner
+            node = self
+            while node is not None:
+                node.update(winner)
+                node = node.parent
+
+            return
+
+        child = self.select_child(temp)
+        child.simulate(temp)
+
+    def select_move(self, resign_rate: float = -1.0) -> Move:
+        """
+        Select best move.
+
+        Return resign move if win rate is lower than resign_rate.
+        """
+        best_rate, best_move = resign_rate, Move.resign()
+
+        for child in self.children:
+            win_rate = child.win_rate
+            if win_rate > best_rate:
+                best_rate, best_move = win_rate, child.game.move
+
+        return best_move
+
+
+class MCTSBot(Agent):
+    """Monte Carlo tree search bot."""
+
+    def __init__(
+        self, rounds: int = 100, temp: float = 1.5, resign_rate: float = 0.1
+    ) -> None:
+        super().__init__()
+        self.rounds = rounds
+        self.temp = temp
+        self.resign_rate = 0.1
+
+    def select_move(self, game: Game) -> Move:
+        root = MCTSNode(game)
+
+        for _ in range(self.rounds):
+            root.simulate(self.temp)
+
+        return root.select_move(self.resign_rate)
