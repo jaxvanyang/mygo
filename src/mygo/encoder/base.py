3c05c1476ce3e71758978d86d5d93d76b50f66f1
Jax Young <jaxvanyang@gmail.com>
2024-05-07 09:52:55 +0800

    lint(pytype): disable bad-return-type check for abstract method

--- src/mygo/encoder/base.py
+++ src/mygo/encoder/base.py
@@ -6,7 +6,10 @@ from mygo.game.types import Game
 
 
 class Encoder(ABC):
+    # pytype: disable=bad-return-type
     @staticmethod
     @abstractmethod
     def encode(game: Game) -> ndarray:
         """Return encoded board of game."""
+
+    # pytype: enable=bad-return-type
