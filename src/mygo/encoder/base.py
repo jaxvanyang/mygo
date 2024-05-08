a213955d71a1475d6ca8da46cc76213f8f4afa26
Jax Young <jaxvanyang@gmail.com>
2024-05-08 17:05:07 +0800

    feat(encoder): add function to decode point index

--- src/mygo/encoder/base.py
+++ src/mygo/encoder/base.py
@@ -2,10 +2,14 @@ from abc import ABC, abstractmethod
 
 from numpy import ndarray
 
-from mygo.game.types import Game
+from mygo.game.types import Game, Point
 
 
 class Encoder(ABC):
+    def __init__(self, board_size: int = 19) -> None:
+        super().__init__()
+        self.size = board_size
+
     # pytype: disable=bad-return-type
     @staticmethod
     @abstractmethod
@@ -13,3 +17,11 @@ class Encoder(ABC):
         """Return encoded board of game."""
 
     # pytype: enable=bad-return-type
+
+    def decode_point_index(self, idx: int) -> Point:
+        idx = int(idx)
+        assert 0 <= idx < self.size**2
+
+        row = idx // self.size + 1
+        col = idx % self.size + 1
+        return Point(row, col)
