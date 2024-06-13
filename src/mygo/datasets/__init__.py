3f0381f33bd657282f829ca9ae0489b077e1ca3b
Jax Young <jaxvanyang@gmail.com>
2024-06-13 13:23:35 +0800

    feat(dataset): add KGSIterableDataset

--- src/mygo/datasets/__init__.py
+++ src/mygo/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .mcts import MCTSDataset
-from .sgf import KGSDataset
+from .sgf import KGSDataset, KGSIterableDataset
 
-__all__ = ["MCTSDataset", "KGSDataset"]
+__all__ = ["MCTSDataset", "KGSDataset", "KGSIterableDataset"]
