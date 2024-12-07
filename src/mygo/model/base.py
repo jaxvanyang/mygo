66bf9c0f43631c5a51aa9f0ce237a6bcb1466c45
Jax Young <jaxvanyang@gmail.com>
2024-12-07 22:50:12 +0800

    feat(model): Model: add print_info()

--- src/mygo/model/base.py
+++ src/mygo/model/base.py
@@ -10,3 +10,17 @@ class Model(nn.Module):
             if subclass.name == name:
                 return subclass
         raise ValueError(f"Unknown model: {name}")
+
+    def print_info(self):
+        device = next(self.parameters()).device
+        n_params = sum(p.numel() for p in self.parameters())
+
+        print(
+            "\n".join(
+                [
+                    f"Device: {device}",
+                    f"Parameters: {n_params:,d}",
+                    f"Structure:\n{self}",
+                ]
+            )
+        )
